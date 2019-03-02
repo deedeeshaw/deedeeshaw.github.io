a7f97054cca071e751f3121b4cdebe21332008c0
deedee <dinshawdiana@gmail.com>
2019-03-02 10:14:31 -0700

    apps not connecting

--- Mars/app.py
+++ Mars/app.py
@@ -1,23 +1,23 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, Response, request, url_for
 from flask_pymongo import PyMongo
 import scrape_mars
 
 app= Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-mongo=PyMongo(app, uri= 'mongo://localhost:27017/mars_db' )
+mongo=PyMongo(app, uri='mongo://localhost:27017/mars_db')
 
 @app.route('/')
 def index():
-    mars =mongo.db.mars.find_one()
+    mars = mongo.db.mars.find()
     return render_template('index.html', mars=mars)
 
-@app.route('/scrape')
+@app.route('/scrape/', methods=['GET'])
 def scrape():
     mars= mongo.db.mars
     mars_data = scrape_mars.scrape()
     mars.update({}, mars_data, upsert=True)
-    return redirect ('/', code=302)
+    return render_template('index.html', mars_data=mars)
 
 if __name__=='__main__':
     app.run(debug=True)
