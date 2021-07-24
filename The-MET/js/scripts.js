b351889aa4f46ce217588d78336740a8c9afb3b1
deedee <dinshawdiana@gmail.com>
2021-07-24 12:26:33 -0700

    js api and random number

--- The-MET/js/scripts.js
+++ The-MET/js/scripts.js
@@ -3,5 +3,22 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+async function getApi() {
+    randomId = Math.floor(Math.random()*4000);
+    console.log(randomId);
+    let metUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + randomId;
+    console.log(metUrl);
+    try {
+        let result = await fetch(metUrl);
+        return await result.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+async function renderApi() {
+    console.log('start here')
+    let object = await getApi();
+    console.log(object.accessionYear, object.department, object.objectName, object.title, object.artistDisplayName)
+}
